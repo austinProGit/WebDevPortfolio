0984cefbae70b06498e9acd207f5deeb2f153499
austinProGit <97987573+austinProGit@users.noreply.github.com>
2023-04-20 15:12:45 -0400

    warning about free hosting

--- src/components/Projects/TrashBusters.jsx
+++ src/components/Projects/TrashBusters.jsx
@@ -11,6 +11,8 @@ const TrashBusters = () => {
     It allows users who see litter in their community to drop pins for needed cleanup on an embedded Google Map, add 
     descriptions for cleanup sites, and delete pins once trash sites have been cleaned. Developed skills include: MERN stack, 
     multi-repo integrations, deployment, environment variables, and automated testing.
+
+    ***Please note: due to free testing, this app may take up to five minutes to load.***
     </p>
     <p className='project-description'>Check out my project hosted <a href="https://trashbusters-3ag6.onrender.com">on Render</a></p>
     
